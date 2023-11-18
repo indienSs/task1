b28158bde698addfff25a127541580ae05dcac5c
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-11-18 21:54:24 +0300

    routes

---
+++ src/main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <p>Необходимо войти в аккаунт</p>
+      <a href="/login">
+        <button>Войти</button>
+      </a>
+    </div>
+  );
+}
