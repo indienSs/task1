b06e6b95b7ed2f480a7a0c10094d63e1ffd2f818
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-11-19 15:46:56 +0300

    localStorage

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,22 +3,31 @@ import PageLayout from "../../layouts/page-layuot";
 import styles from "./Login.module.css";
 import { fetchData } from "../../utils/fetchData";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
 
   const changeLogin = e => setLogin(e.target.value);
   const changePassword = e => setPassword(e.target.value);
-  const submitForm = () => {
-    fetchData({ login, password }).then(res => console.log(res));
+  const submitForm = e => {
+    e.preventDefault();
+    fetchData({ login, password }).then(res => {
+      if (res.ok) {
+        navigate("/");
+        window.localStorage.setItem("user", login);
+      }
+    });
   };
 
   return (
     <PageLayout>
       <form className={styles.login} onSubmit={submitForm}>
-        <input type="email" onChange={changeLogin} />
-        <input type="password" onChange={changePassword} />
+        <input type="email" name="login" value={login} onChange={changeLogin} />
+        <input type="password" name="password" value={password} onChange={changePassword} />
         <button type="submit">Войти</button>
       </form>
     </PageLayout>
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PageLayout from "../../layouts/page-layuot";
 
 export default function Main() {
+  const [user, setUser] = useState(window.localStorage.getItem("user"));
+
   return (
     <PageLayout>
-      <p>Необходимо войти в аккаунт</p>
-      <a href="/login">
-        <button>Войти</button>
-      </a>
+      {!user ? (
+        <>
+          <p>Необходимо войти в аккаунт</p>
+          <a href="/login">
+            <button>Войти</button>
+          </a>
+        </>
+      ) : (
+        <>
+          <p>Вы зашли в аккаунт {user}</p>
+        </>
+      )}
     </PageLayout>
   );
 }
