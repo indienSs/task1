61bc427664a0dd5c8d3d1d8be5bc9d20998edff0
Oleg Andrianov <oleg.andrianov@bk.ru>
2023-11-19 15:31:21 +0300

    promise

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,3 @@
 export function fetchData({ login, password }) {
-  return new Promise((res, rej) => {
-    setTimeout(() => res({ ok: true, login }), 1000);
-  });
+  return new Promise((res, rej) => setTimeout(() => res({ ok: true, login }), 1000));
 }
